4af8458ab68b6d240616c69c404cb53e083d294a
Jonathan Beaubien <beaubj@uw.edu>
2022-02-21 21:17:04 -0800

    margin

--- src/index.js
+++ src/index.js
@@ -242,7 +242,7 @@ class Game extends React.Component {
         >
           <SuggestionForm />
           <button
-            className="mt-10 text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+            className="mt-10 mb-5 text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
             type="button"
             onClick={() => this.setPage(1)}
           >
